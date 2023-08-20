4d4656904a186df3d9095ecbb8610aa5072ac236
Simonas Zilevicius <s.zilevicius@gmail.com>
2023-08-20 18:32:27 +0300

    Word builder will be wrong, but want to check the output

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        if (power < 0) {
+            System.out.println("too much power");
+        }
+        int i = 0;
+        int answer = 1;
+        while (i <= power) {
+            System.out.println(answer);
+            answer *= 2;
+            i += 1;
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-
+        String text = "";
+        for (int i = 0; i < chars.length; i++) {
+            text += chars[i];
+        } System.out.println(text);
     }
 }
+
+
+
